3fe0fe2bee0505981040d0e8aa49edad2154c84d
Dianapalacios260105 <20230082@uthh.edu.mx>
2025-02-06 22:26:49 -0600

    first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>P A G A R O</Text>
       <StatusBar style="auto" />
     </View>
   );
